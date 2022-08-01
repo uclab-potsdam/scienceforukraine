77f24f0c88f00a7fd1796b61654b323054a35904
fidelthomet <git@ft0.ch>
2022-08-01 10:12:29 +0200

    fixed routing scrollbehaviour

--- src/router/index.js
+++ src/router/index.js
@@ -90,7 +90,11 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  }
 })
 
 router.beforeEach((to, from, next) => {
