d25a9616651ef45050aecc6bb583d227f92aa40c
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2023-05-22 20:02:26 +0200

    Rename funding button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
       <h2>I Look for Help</h2>
       <div class="navigation">
         <router-link class="primary" to="/listings">View All Support Listings</router-link>
-        <router-link to="/support">Archive of<br>Funding Programmes</router-link>
+        <router-link to="/support">Funding programs<br>and other support</router-link>
       </div>
     </div>
     <div class="content">
